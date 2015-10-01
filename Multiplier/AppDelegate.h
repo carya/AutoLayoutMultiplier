4b27ff11ddeb93b88a94a67ef8842b5900f864aa
carya <carya.liu@gmail.com>
2015-10-01 16:42:33 +0800

    first commit

---
+++ Multiplier/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Multiplier
+//
+//  Created by MaohuaLiu on 15/9/27.
+//  Copyright © 2015年 CaryaLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Multiplier/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multiplier
+//
+//  Created by MaohuaLiu on 15/9/27.
+//  Copyright © 2015年 CaryaLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
